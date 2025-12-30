6128f24aa0823a719b9d58b446f508c8acdead13
Keisuke Tanabe <k.eugene.t@gmail.com>
2025-12-30 09:23:29 +0900

    Lesson16 header guard

---
+++ 16-header-guard/header-guard-1/Player.hpp
@@ -0,0 +1,11 @@
+#ifndef PLAYER_HPP      // もしPLAYER_Hがまだ定義されていなければ
+#define PLAYER_HPP      // PLAYER_Hを定義する
+
+class Player
+{
+public:
+    void Move();
+    void Attack();
+};
+
+#endif  // PLAYER_HPP
